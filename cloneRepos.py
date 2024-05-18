ae3692caefcad5d1c7758ecdacf2f3dc848a2ea2
Pedro-nll <pedronll@outlook.com>
2024-05-18 16:50:46 -0300

    Feat: cloneRepos refactoring

--- cloneRepos.py
+++ cloneRepos.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime
 from git import Repo
 from git.remote import RemoteProgress
 
@@ -8,7 +8,7 @@ class Progress(RemoteProgress):
     def update(self, *args):
         print(self._cur_line)
 
-# Define the list of repositories to be analyzed
+# Lista de repos escolhida para o mini-artigo
 repos = {
     "public-apis/public-apis": "https://github.com/public-apis/public-apis",
     "AUTOMATIC1111/stable-diffusion-webui": "https://github.com/AUTOMATIC1111/stable-diffusion-webui",
@@ -18,26 +18,14 @@ repos = {
     "pytorch/pytorch": "https://github.com/pytorch/pytorch"
 }
 
-GITHUB_TOKEN = ""
+GITHUB_TOKEN = "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"
 
-def get_commits(repo_name):
+def get_commits(repo_name, per_page=30):
     url = f"https://api.github.com/repos/{repo_name}/commits"
     headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, params={"per_page": per_page})
     response.raise_for_status()
     return response.json()
-
-def get_version_dates():
-    today = datetime.today()
-    six_months_ago = today - timedelta(days=180)
-    return today.strftime('%Y-%m-%d'), six_months_ago.strftime('%Y-%m-%d')
-
-def filter_commits_by_date(commits, desired_date):
-    for commit in commits:
-        commit_date = datetime.strptime(commit['commit']['author']['date'], '%Y-%m-%dT%H:%M:%SZ').date()
-        if commit_date <= desired_date:
-            return commit['sha']
-    return None
 
 def clone_repo(repo_url, clone_dir):
     if os.path.exists(clone_dir):
@@ -51,41 +39,44 @@ def checkout_version(repo_dir, commit_sha):
     repo.git.checkout(commit_sha)
 
 def main():
+    # repos.items são todos os repositorios selecionados a partir do script pythonRepos.py
+    # repo_name é o dono/nome do repo e url é a url de acesso
     for repo_name, repo_url in repos.items():
         print(f"Processing: {repo_name}")
+        # get_commits pega os ultimos 30 commits de um dos repo
+        # 30 é a paginação do github
         commits = get_commits(repo_name)
         
-        # Get version dates
-        today_str, six_months_ago_str = get_version_dates()
-        today_date = datetime.strptime(today_str, '%Y-%m-%d').date()
-        six_months_ago_date = datetime.strptime(six_months_ago_str, '%Y-%m-%d').date()
+        if not commits:
+            print(f"No commits found for {repo_name}")
+            continue
+
+        # primeiro comit (numero 29) da lista de commits e o ultimo commit feito (0) 
+        first_commit_sha = commits[-1]['sha']
+        latest_commit_sha = commits[0]['sha']
         
-        # Filter commits by date
-        latest_commit_sha = filter_commits_by_date(commits, today_date)
-        older_commit_sha = filter_commits_by_date(commits, six_months_ago_date)
-        
-        # Define directories for versions
+        # Definindo o nome dos arquivos pra cada versão
         base_clone_dir = f"./repos/{repo_name}"
         latest_dir = f"{base_clone_dir}_latest"
-        older_dir = f"{base_clone_dir}_older"
+        first_dir = f"{base_clone_dir}_first"
         
-        # Clone the repository and checkout the latest version
+        # Clona o repo e dá checkout pra ultima versão dele
         if latest_commit_sha and not os.path.exists(latest_dir):
             clone_repo(repo_url, base_clone_dir)
             if not os.path.exists(latest_dir):
                 os.mkdir(latest_dir)
             checkout_version(base_clone_dir, latest_commit_sha)
             os.rename(base_clone_dir, latest_dir)
-            print(f"Latest version saved in {latest_dir}")
+            print(f"ULTIMA VERSÃO SALVA NO PATH {latest_dir}")
 
-        # Checkout the older version
-        if older_commit_sha and not os.path.exists(older_dir):
+        # Clona o repo e dá checkout pra versão mais antiga pega na paginação dele
+        if first_commit_sha and not os.path.exists(first_dir):
             clone_repo(repo_url, base_clone_dir)
-            if not os.path.exists(older_dir):
-                os.mkdir(older_dir)
-            checkout_version(base_clone_dir, older_commit_sha)
-            os.rename(base_clone_dir, older_dir)
-            print(f"Older version saved in {older_dir}")
+            if not os.path.exists(first_dir):
+                os.mkdir(first_dir)
+            checkout_version(base_clone_dir, first_commit_sha)
+            os.rename(base_clone_dir, first_dir)
+            print(f"PRIMEIRA VERSAO SALVA NO PATH {first_dir}")
 
 if __name__ == "__main__":
     main()
